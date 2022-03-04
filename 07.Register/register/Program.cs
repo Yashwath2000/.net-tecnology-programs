08f9a06471c94edfebbe7daae97dddb25703ccf9
Yashwath2000 <98145017+Yashwath2000@users.noreply.github.com>
2022-03-04 12:18:30 +0530

    Add files via upload

---
+++ 07.Register/register/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace register
+{
+    class RegisterNum
+    {
+        int regno;
+        static int startNum;
+        static RegisterNum()
+        {
+            startNum = 20210000;
+        }
+        RegisterNum()
+        {
+            regno = ++startNum;
+        }
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                RegisterNum student = new RegisterNum();
+                Console.WriteLine("student {0}:{1}", i + 1, student.regno);
+            }
+        }
+    }
+}
