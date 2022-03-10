85593e83b4edd0cddb08b4e4734c6bf10b7c2fdb
Yashwath2000 <98145017+Yashwath2000@users.noreply.github.com>
2022-03-10 14:54:15 +0530

    Add files via upload

---
+++ 15.Error_handling/error_handling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace error_handling
+{
+    class Exceptionhandling
+    {
+        static void Main(string[] args)
+        {
+            Age a = new Age();
+            try
+            {
+                a.displayAge();
+            }
+            catch(AgeIsNegativeException e)
+            {
+                Console.WriteLine("AgeIsNegativeException:{0}", e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("execution of finally block is done.");
+            }
+        }
+    }
+}
+public class AgeIsNegativeException : Exception
+{
+    public AgeIsNegativeException(string message) : base(message)
+    {
+    }
+}
+public class Age
+{
+    int age =-5;
+    public void displayAge()
+    {
+        if (age < 0)
+        {
+            throw (new AgeIsNegativeException("Age cannot be negative"));
+        }
+        else
+        {
+            Console.WriteLine("Age is: {0}", age);
+        }
+    }
+}
+
